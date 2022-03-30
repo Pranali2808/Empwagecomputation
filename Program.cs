dc51c3110fc6ade5a430ac476b7019810bcb0f09
pranali2808 <ranepranali1997@gmail.com>
2022-03-30 20:29:21 +0530

    UC6: wages till a condition of total working hours or days is reached for a month

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Empolyee Wage Computation Program\n");
+            EmpAttendance.GetEmpAttendance();
+            DailyEmpWage.GetDailyEmpWage();
+            ParttymEmpWage.GetParttymEmpWage();
+            SwitchCase.DailyEmpWage();
+            MonthWage.GetMonthWage();
+            TotalWage.GetTotalWage();
+            Console.ReadLine();
+        }
+    }
+}
